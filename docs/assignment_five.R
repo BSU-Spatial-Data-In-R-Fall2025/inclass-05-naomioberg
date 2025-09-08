99f40726c5794d56b8f7e7020d6137df23b4b151
Naomi Oberg <naomioberg@u.boisestate.edu>
2025-09-08 20:44:24 +0000

    Subsetting the datasets to only the variables of interest.

--- docs/assignment_five.R
+++ docs/assignment_five.R
@@ -20,3 +20,28 @@ problems(risch_receipts)
 #what is row 17? - recipient)_committee_org_type
 names(risch_receipts)[17]
 
+# Selecting the variables of interest
+select_vars <- c("is_individual", "contributor_name", "contribution_receipt_amount",
+                 "committee_name", "report_year")
+
+simpson_subset <- simpson_receipts %>% 
+  select(all_of(select_vars)) %>% 
+  filter(is_individual == TRUE)
+
+fulcher_subset <- fulcher_receipts %>% 
+  select(all_of(select_vars)) %>% 
+  filter(is_individual == TRUE)
+
+crapo_subset <- crapo_receipts %>% 
+  select(all_of(select_vars)) %>% 
+  filter(is_individual == TRUE)
+
+risch_subset <- risch_receipts %>% 
+  select(all_of(select_vars)) %>% 
+  filter(is_individual == TRUE)
+
+glimpse(simpson_subset)
+  
+  
+  
+  
