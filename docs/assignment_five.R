8303ad43ea0b390189d3eda3deeff9cf6af1b4d1
Naomi Oberg <naomioberg@u.boisestate.edu>
2025-09-08 20:40:12 +0000

    Troubleshooting issues with column 17 in the Risch dataset

--- docs/assignment_five.R
+++ docs/assignment_five.R
@@ -8,3 +8,15 @@ simpson_receipts <- read_delim("data/original/simpson_fec_17_26.csv",
 fulcher_receipts <- read_delim("data/original/fulcher_fec_17_26.csv",
                                delim = ",")
 
+crapo_receipts <- read_delim("data/original/crapo_fec_17_26.csv",
+                             delim = ",")
+
+risch_receipts <- read_delim("data/original/risch_fec_17_26.csv",
+                             delim = ",")
+
+#troubleshooting problems with risch dataset
+problems(risch_receipts)
+#there is a problem with the 17th column
+#what is row 17? - recipient)_committee_org_type
+names(risch_receipts)[17]
+
